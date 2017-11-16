5c45ccba13e088655af07eceeee1c881436098d3
hocklo <ecollvila@gmail.com>
2017-11-16 22:54:29 +0100

    Init repository

---
+++ server.lua
@@ -0,0 +1,14 @@
+local http = require('http')
+local json = require('json').use_lpeg()
+
+http.createServer(function (req, res)
+
+  local tbl = { greeting = "Hello World!" }
+  local body = json.stringify( tbl )
+--  local body = "Hello world\n"
+  res:setHeader("Content-Type", "text/plain")
+  res:setHeader("Content-Length", #body)
+  res:finish(body)
+end):listen(1337, '127.0.0.1')
+
+print('Server running at http://127.0.0.1:1337/')
